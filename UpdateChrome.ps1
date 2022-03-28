5db1b1e488806b5cf317274ad818a7ccad040b01
Solex <76534423+AlexHailstone@users.noreply.github.com>
2022-03-28 11:00:36 -0400

    Add files via upload

---
+++ UpdateChrome.ps1
@@ -0,0 +1,21 @@
+$VersionCheck = Get-WmiObject -Class Win32_Product | where name -eq 'google chrome' | select Version
+Write-Host "Current version:" $VersionCheck
+If ($VersionCheck -ge 99.0.4844.84){
+    
+    ## If Winget is working
+    $WingetInstalled = Get-AppxPackage | findstr "Microsoft.Winget.Source" 
+    If ($WingetInstalled -ne "") {
+        Write-Host "Winget Is installed, Updating now..."
+        winget upgrade -h chrome
+        taskkill /F /IM chrome.exe
+    } else {
+        Write-Host "Winget is not installed, using MSIExec"
+        Invoke-WebRequest -URi "https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi" -OutFile "C:\temp\UpgradeChrome.msi"
+        C:\temp\UpgradeChrome.msi /qn
+        taskkill /F /IM chrome.exe
+        $NewVersion = Get-WmiObject -Class Win32_Product | where name -eq 'google chrome' | select Version
+        Write-Host $NewVersion
+    }
+} Else {
+    Write-host "Chrome is Updated"
+}
